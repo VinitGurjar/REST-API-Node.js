4b8a2992262c4fef0507e827281de7b0802bd725
adhiramaa <vinitfagna@gmail.com>
2023-03-19 00:48:45 +0530

    Added the 2 Endpoints for the API

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const app = require("express")();
 const PORT = 8080;
 
+//Defined a port for on server to listen our API
+app.listen(PORT, () => console.log(`he's alive on http://localhost:${PORT}`));
+
 //creating an endpoint
 app.get(
   "/some" /*ROUTE*/,
@@ -13,3 +16,6 @@ app.get(
     });
   } /*HANDLER*/
 );
+
+//Post Endpoint with a Route parameters for capturing synamic values in the URL
+app.post("/some/:id ");
